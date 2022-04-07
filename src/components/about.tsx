fa0ab2646b657d3cdaf84ad1d5977a835d71fb4b
Miffy Wang <4000p049@gmail.com>
2022-04-07 12:10:31 +0800

    add margin

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -19,6 +19,7 @@ const StartImg = styled.img`
 
 const TopArea = styled.div`
   width:100%;
+  display:flex;
 
 `;
 
@@ -35,16 +36,16 @@ const LeftItems = styled.div`
  flex-direction: column;
  width:50%;
  
-
-
 `;
 
 const RightItems = styled.div`
  display:flex;
+ flex-direction: column;
  width:50%;
-
-
+ 
+ 
 `;
+
 
 const belowLeftItems = styled.div`
 
@@ -113,10 +114,18 @@ const AboutItems = styled.div`
 
 `;
 
-const TopAreaItems = styled.div`
+const LeftAreaContainer = styled.div`
   width:100%;
   display: flex;
-  justify-content:center;
+  justify-content:flex-end;
+  margin-right:40px;
+  
+`;
+const RightAreaContainer = styled.div`
+  width:100%;
+  display: flex;
+  justify-content:flex-start;
+  margin-left:40px;
   
 `;
 
@@ -169,7 +178,7 @@ const About = () => {
         <AboutUs>About Us</AboutUs>
       </AboutItems>
       <TopArea>
-        <TopAreaItems>
+        <LeftAreaContainer>
           <LeftItems>
             <HopeContainer>
             <StartImg src={start} alt="" />
@@ -189,11 +198,34 @@ const About = () => {
               </div>
             </BrandContainer>
 
-
           </LeftItems>
      
-        </TopAreaItems>
+        </LeftAreaContainer>
 
+
+        <RightAreaContainer>
+          <RightItems>
+            <HopeContainer>
+              <StartImg src={start} alt="" />
+              <div>
+                <Hope>願景</Hope>
+                <Slogan>Let's Play <Span>Dream</Span></Slogan>
+                <Contants>我們有個小小的願望，就是和大家一起「玩」桌球，透過輕鬆玩，讓全身細胞體會動起來的暢快感！我們有個大大的夢想，藉由「玩」桌球，跨越單純的運動本身、與各種可能性結合，讓桌球走進你我的生活中，帶著爸爸、媽媽、阿公、阿嬤、叔叔、伯伯、阿姨、姑姑、哥哥、姊姊、弟弟、妹妹、朋友所有人一起 Let's Play！</Contants>
+              </div>
+            </HopeContainer>
+            <BrandContainer>
+              <StartImg src={start} alt="" />
+              <div>
+                <Hope>品牌理念</Hope>
+                <Slogan>Let's Play <Span>Thinking</Span></Slogan>
+                <Contants>感受發自內心的快樂，一起來玩吧！
+                  品牌的核心價值在於「讓來到 Let's Play 的人能發自內心的感受純粹的快樂」，孩子從中獲得專注力與成就感、上班族重新找回喜愛一件事物的單純與雀躍感、長者獲得豐富退休生活的歸屬感。</Contants>
+              </div>
+            </BrandContainer>
+
+          </RightItems>
+
+        </RightAreaContainer>
         
       </TopArea>
     </Container>
