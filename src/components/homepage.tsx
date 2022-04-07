7a528a9c89c724d95956c5feeae7b3a465860ba3
Miffy Wang <4000p049@gmail.com>
2022-04-07 12:26:50 +0800

    homepage settting

---
+++ src/components/homepage.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import styled from "styled-components";
+import start from "../assets/about/start.png";
+
+
+
+// styled components start 
+
+const PageContainer = styled.div`
+  height: 100vh;
+  width:100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+
+
+
+/* desktop thw newest info style end */
+const homepage = () => {
+  return <PageContainer>
+
+
+
+
+  </PageContainer>;
+};
+
+export default homepage;
