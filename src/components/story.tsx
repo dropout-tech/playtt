62359af0d9765f4cc639c053abce6f0f7ef2bd03
Miffy Wang <4000p049@gmail.com>
2022-04-07 13:34:19 +0800

    edit text

--- src/components/story.tsx
+++ src/components/story.tsx
@@ -24,12 +24,14 @@ const ColorpicImg = styled.img`
 const Container = styled.div`
  width:100% ;
  height:920px;
+ display:flex;
 `;
 
 const Picture = styled.div`
   width:50%;
   display:flex;
   justify-content:flex-end;
+  margin-top:90px;
 
 
 `;
@@ -38,16 +40,42 @@ const Text = styled.p`
   width:50%;
   display:flex;
   justify-content:flex-start;
-
+  flex-direction: column;
+  margin-left:80px;
+  margin-top:-80px;
 `;
 
 const Title1 = styled.p`
-  
+  font-family: 'Noto Sans TC';
+font-style: normal;
+font-weight: 900;
+font-size: 30px;
+line-height: 44px;
+/* identical to box height, or 147% */
+
+letter-spacing: 0.04em;
+
+/* black */
+
+color: #1A1A1A;
+
 
 `;
 
 const Title2 = styled.p`
-  
+  font-family: 'Futura';
+font-style: normal;
+font-weight: 700;
+font-size: 26px;
+line-height: 40px;
+/* identical to box height, or 154% */
+
+letter-spacing: 0.04em;
+
+/* black */
+
+color: #1A1A1A;
+
 
 `;
 
@@ -62,6 +90,38 @@ const Title4 = styled.p`
 `;
 
 const Contant = styled.p`
+  width:517px;
+  font-family: 'Futura';
+font-style: normal;
+font-weight: 500;
+font-size: 20px;
+line-height: 36px;
+/* or 180% */
+
+text-align: justify;
+letter-spacing: 0.03em;
+
+/* black */
+
+color: #1A1A1A;
+
+`;
+
+const BelowText = styled.p`
+  font-family: 'Futura';
+font-style: normal;
+font-weight: 700;
+font-size: 26px;
+line-height: 40px;
+/* or 154% */
+
+letter-spacing: 0.04em;
+
+/* black */
+
+color: #1A1A1A;
+
+
 
 `;
 
@@ -73,25 +133,29 @@ const Contant = styled.p`
 /* desktop thw newest info style end */
 const Story = () => {
   return <PageContainer>
+    <Container>
+      <Picture>
+        <ColorpicImg src={colorpic} alt="" />
+      </Picture>
 
-    <Picture>
-      <ColorpicImg src={colorpic} alt="" />
-    </Picture>
 
-    <Text>
-      <div>
-        <Title1>我們的故事</Title1>
-        <Title2>Let’s Play Story</Title2>
-        <Contant>Let's Play 創立於 2018 年，創辦人阿寬教練畢業於桌球名校麗山國中與松山家商，大學時期深受林學宜教授的啟發，非常懂得職業球員訓練的辛苦，但他其實更想分享給大家的是，如何和大家快樂的「玩」桌球，一起感受其中的樂趣！小孩子來到這裡可以培養信心與專注力、上班族可以擁有用運動釋放壓力的時光、年長者可以實現豐富退休生活的樂趣！</Contant>
+      <Text>
+        <div>
+          <Title1>我們的故事</Title1>
+          <Title2>Let’s Play Story</Title2>
+          <Contant>Let's Play 創立於 2018 年，創辦人阿寬教練畢業於桌球名校麗山國中與松山家商，大學時期深受林學宜教授的啟發，非常懂得職業球員訓練的辛苦，但他其實更想分享給大家的是，如何和大家快樂的「玩」桌球，一起感受其中的樂趣！小孩子來到這裡可以培養信心與專注力、上班族可以擁有用運動釋放壓力的時光、年長者可以實現豐富退休生活的樂趣！</Contant>
 
-        <Contant>我們期望讓人感受像是回到家一般的親切，教練們如同鄰居大哥哥一樣帶著大家一起玩！根據不同水平慢慢地引導，用簡單的口訣輕鬆的氣氛中自然而然地上手，原來，桌球一點也不難！此外，從桌球同業的夥伴聯盟合作與跨領域的異業結合，到建立桌球教練創業輔導的平台，甚至邀請名人一起玩桌球，越來越多的創意可能性等著我們去實現！</Contant>
-      </div>
+          <Contant>我們期望讓人感受像是回到家一般的親切，教練們如同鄰居大哥哥一樣帶著大家一起玩！根據不同水平慢慢地引導，用簡單的口訣輕鬆的氣氛中自然而然地上手，原來，桌球一點也不難！此外，從桌球同業的夥伴聯盟合作與跨領域的異業結合，到建立桌球教練創業輔導的平台，甚至邀請名人一起玩桌球，越來越多的創意可能性等著我們去實現！</Contant>
+        </div>
 
-      <div>
-        <Title3>Let's Play 運動 X 創新</Title3>
-        <Title4>邀請大家一起來玩！</Title4>
-      </div>
-    </Text>
+        <BelowText>
+          <Title3>Let's Play 運動 X 創新</Title3>
+          <Title4>邀請大家一起來玩！</Title4>
+        </BelowText>
+      </Text>
+
+
+    </Container>
 
 
 
