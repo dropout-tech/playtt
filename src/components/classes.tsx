6ea0478d8a9a05cd0a7010627d9a9ac9e13a0f26
Miffy Wang <4000p049@gmail.com>
2022-04-07 14:34:22 +0800

    class introduce setting

--- src/components/classes.tsx
+++ src/components/classes.tsx
@@ -8,12 +8,55 @@ const PageContainer = styled.div`
   justify-content: space-between;
 `;
 
+const Container = styled.div`
+  width:100%;
+`;
+
+
+const ClassIntroduce = styled.div`
+
+`;
+
+const Title1 = styled.p`
+  font-family: 'Noto Sans TC';
+  font-style: normal;
+  font-weight: 900;
+  font-size: 36px;
+  line-height: 52px;
+  text-align: center;
+  letter-spacing: 0.04em;
+  color: #1A1A1A;
+`;
+
+const Title2 = styled.p`
+  font-family: 'Futura';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 36px;
+  line-height: 48px;
+  text-align: center;
+  letter-spacing: 0.04em;
+  color: #CFD2D3;
+`;
+
+
 /* desktop thw newest info style end */
 const Classes = () => {
   return <PageContainer>
+    <Container>
+
+      <ClassIntroduce>
+        <Title1>課程介紹</Title1>
+        <Title2>Our Class</Title2>
+      </ClassIntroduce>
+
+      <div></div>
 
 
-    
+
+    </Container>
+
+
   </PageContainer>;
 };
 
