1f3b8ae3c292b5a907416133d8d7dc4e5e45de9b
Miffy Wang <4000p049@gmail.com>
2022-04-08 15:12:47 +0800

    setting background

--- src/components/partnershipAndRecruit.tsx
+++ src/components/partnershipAndRecruit.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-
+import colorLine from "../assets/partnershipAndRecruit/colorLine.png";
 
 
 
@@ -14,12 +14,64 @@ const PageContainer = styled.div`
   justify-content: space-between;
 `;
 
+const Title1 = styled.p`
+  font-family: 'Noto Sans TC';
+  font-style: normal;
+  font-weight: 900;
+  font-size: 36px;
+  line-height: 52px;
+  text-align: center;
+  letter-spacing: 0.04em;
+  color: #1A1A1A;
+`;
+
+const Title2 = styled.p`
+  font-family: 'Futura';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 36px;
+  line-height: 48px;
+  text-align: center;
+  letter-spacing: 0.04em;
+  color: #CFD2D3;
+`;
+
+const Container = styled.div`
+  width: 100%;
+  height:100%;
+
+
+`;
+
+const TitleContainer = styled.div`
+  margin-top:500px;
+
+`;
+
+const BlueArea = styled.div`
+  width: 100%;
+  height:80%;
+  background: #005CB9;  
+`;
+
+
+
 
 
 /* desktop thw newest info style end */
 const PartnershipAndRecruit = () => {
   return <PageContainer>
- 
+    <Container>
+      <TitleContainer>
+        <Title1>合作與招募</Title1>
+        <Title2>Partnership & Recruit</Title2>
+      </TitleContainer>
+
+      <BlueArea>
+
+      </BlueArea>
+    </Container>
+
   </PageContainer>;
 };
 
