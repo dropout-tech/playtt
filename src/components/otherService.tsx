a9541ccc28db6deb31f5ba46d211b516a0846a45
Miffy Wang <4000p049@gmail.com>
2022-04-11 12:03:17 +0800

    fix

--- src/components/otherService.tsx
+++ src/components/otherService.tsx
@@ -18,30 +18,66 @@ const Container = styled.div`
 
 `;
 
+const BelowArea = styled.div`
+ display: flex;
+ justify-content:center;
+ margin-top:30px; 
+`;
+
 const Title1 = styled.p`
   margin-top:50px;
   display: flex;
   justify-content:center;
+  font-family: 'Noto Sans TC';
+  font-style: normal;
+  font-weight: 900;
+  font-size: 36px;
+  line-height: 52px;
+  text-align: center;
+  letter-spacing: 0.04em;
+  color: #1A1A1A;
 `;
 
 const Title2 = styled.p`
   display: flex;
   justify-content:center;
+  font-family: 'Futura';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 36px;
+  line-height: 48px;
+  text-align: center;
+  letter-spacing: 0.04em;
+  color: #CFD2D3;
+  margin-top:-25px;
 `;
 
 const Title3 = styled.p`
-  
+  font-family: 'Noto Sans TC';
+  font-style: normal;
+  font-weight: 900;
+  font-size: 26px;
+  line-height: 40px;
+  letter-spacing: 0.04em;
+  color: #1A1A1A;
 `;
 
 const TopArea = styled.div`
  display: flex;
  justify-content:center;
- margin-top:80px;
+ margin-top:60px;
 
 `;
 
 const Text = styled.p`
-
+  font-family: 'Noto Sans TC';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 32px;
+  text-align: justify;
+  letter-spacing: 0.04em;
+  color: #1A1A1A;
 `;
 
 
@@ -52,6 +88,8 @@ const TextContainer = styled.div`
   width: 373px;
   height:373px;
   margin-left:30px;
+  padding-left:40px;
+  padding-top:10px;
 `;
 
 /* desktop thw newest info style end */
@@ -102,6 +140,50 @@ const OtherService = () => {
         </TextContainer>
 
       </TopArea>
+
+      <BelowArea>
+        <TextContainer>
+          <Title3>場館加盟與聯盟合作</Title3>
+          <Text>
+            提供系統化的 SOP 協助桌球產業發<br />
+            展更全面，並且共享產業資源互利，<br />
+            提供有心推廣桌球的同業更好的機會。<br />
+          </Text>
+
+          <img src="" alt="" />
+
+        </TextContainer>
+
+
+
+        <TextContainer>
+          <Title3>趣味活動</Title3>
+          <Text>
+            桌球結合趣味競賽<br />
+            各式行銷活動協辦<br />
+            自媒體 / 媒體合作
+          </Text>
+          <img src="" alt="" />
+
+        </TextContainer>
+
+
+
+        <TextContainer>
+          <Title3>VIP 專區</Title3>
+          <Text>
+            特別精心打造的桌球 VIP 包廂空間，<br />
+            讓玩樂和歡笑回憶留在美好的空間，<br />
+            更方便拍照留念。目標成為雙北桌球<br />
+            跨足休閒娛樂的業界首選，<br />
+            開創桌球產業藍海。
+          </Text>
+          <img src="" alt="" />
+
+        </TextContainer>
+
+      </BelowArea>
+
 
 
 
