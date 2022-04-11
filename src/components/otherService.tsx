b974b67ab462cffc025276b77332cefc7ff05c03
Miffy Wang <4000p049@gmail.com>
2022-04-11 11:52:08 +0800

    add text

--- src/components/otherService.tsx
+++ src/components/otherService.tsx
@@ -18,37 +18,97 @@ const Container = styled.div`
 
 `;
 
+const Title1 = styled.p`
+  margin-top:50px;
+  display: flex;
+  justify-content:center;
+`;
+
+const Title2 = styled.p`
+  display: flex;
+  justify-content:center;
+`;
+
+const Title3 = styled.p`
+  
+`;
+
+const TopArea = styled.div`
+ display: flex;
+ justify-content:center;
+ margin-top:80px;
+
+`;
+
+const Text = styled.p`
+
+`;
+
+
+const TextContainer = styled.div`
+  display:flex;
+  flex-direction:column;
+  background: rgba(207, 210, 211,0.2);
+  width: 373px;
+  height:373px;
+  margin-left:30px;
+`;
 
 /* desktop thw newest info style end */
 const OtherService = () => {
   return <PageContainer>
- <Container>
- <div>
-  <p></p>
-  <p></p>
- </div>
+    <Container>
+      <div>
+        <Title1>其他服務</Title1>
+        <Title2>Other Service</Title2>
+      </div>
 
- <div>
+      <TopArea>
+        <TextContainer>
+          <Title3>桌球俱樂部</Title3>
+          <Text>
+            合作機構：華江高中<br />
+            聚集桌球愛好打手<br />
+            舉辦活動分享知識增加凝聚力
+          </Text>
 
-    <div>
-      <p></p>
-      <p></p>
-      <img src="" alt="" />
-    </div>
+          <img src="" alt="" />
 
-
-
-
-
-
- </div>
+        </TextContainer>
 
 
 
+        <TextContainer>
+          <Title3>場地租借</Title3>
+          <Text>
+            平日上午包場優惠<br />
+            歡迎洽談，提早預約<br />
+            平日 14:00–21:00 與周末租桌服務
+          </Text>
+          <img src="" alt="" />
+
+        </TextContainer>
 
 
 
- </Container>
+        <TextContainer>
+          <Title3>球具販售</Title3>
+          <Text>
+            依據需求協助搭配和製作球具，<br />
+            以達到學習和運動成效為主要目的。
+          </Text>
+          <img src="" alt="" />
+
+        </TextContainer>
+
+      </TopArea>
+
+
+
+
+
+
+    </Container>
   </PageContainer>;
 };
 
