4f33484920ea08b66131acea0f9ccbdba843ad7d
Miffy Wang <4000p049@gmail.com>
2022-04-28 18:06:00 +0800

    circle

--- src/components/alliancePlan.tsx
+++ src/components/alliancePlan.tsx
@@ -58,7 +58,7 @@ const TitleWord = styled.p`
 const Background = styled.div`
   background: #F5F6F6;
   width:100%;
-  height:100%;
+  height:804px;
   @media screen and (max-width: 768px) {
 
   }
@@ -68,12 +68,17 @@ const Background = styled.div`
 const Circle1 = styled.div`
   display:flex;
   justify-content:center;
-  width: 450px;
-  height: 450px;
+  width: 588px;
+  height: 588px;
   background: #FFFFFF;
   border-radius:294px;
   margin-top:30px;
   margin-right:-50px;
+  z-index: 1;
+  position: absolute;
+  left: 20px;
+  
+
   @media screen and (max-width: 768px) {
    width: 400px;
    height:400px;
@@ -87,12 +92,12 @@ const Circle1 = styled.div`
 const Circle2 = styled.div`
   display:flex;
   justify-content:center;
-  z-index:1;
-  width: 600px;
-  height: 600px;
+  width: 660px;
+  height: 660px;
   background: #FFFFFF;
   border-radius:330px;
   margin-right:-50px;
+  z-index: 1;
   @media screen and (max-width: 768px) {
     width: 400px;
     height:400px;
@@ -107,11 +112,14 @@ const Circle2 = styled.div`
 const Circle3 = styled.div`
   display:flex;
   justify-content:center;
-  width: 450px;
-  height: 450px;
+  width: 588px;
+  height: 588px;
   background: #FFFFFF;
   border-radius:294px;
   margin-top:20px;
+  z-index: 1;
+  position: absolute;
+  right: 20px;
   @media screen and (max-width: 768px) {
     width: 400px;
     height:400px;
@@ -123,15 +131,37 @@ const Circle3 = styled.div`
   }
 `;
 
-const Text1 = styled.div`
+const Text2 = styled.div`
  margin-top:140px;
+ z-index:10;
+ position: absolute;
+ left: 590px;
+ 
+ 
+ 
+
+ 
  @media screen and (max-width: 768px) {
     margin-top:-30px
   }
 `;
 
-const Text2 = styled.div`
+const Text1 = styled.div`
  margin-top:100px;
+ position: absolute;
+ z-index: 10;
+ left:150px;
+ @media screen and (max-width: 768px) {
+   margin-top:-30px
+  }
+`;
+
+const Text3 = styled.div`
+ margin-top:100px;
+ position: absolute;
+ left: 1030px;
+ z-index: 10;
+
  @media screen and (max-width: 768px) {
    margin-top:-30px
   }
@@ -142,6 +172,8 @@ const Circle = styled.div`
  align-items: center;
  justify-content:center;
  padding:10px;
+ padding-top:70px;
+
  @media screen and (max-width: 768px) {
   display:flex;
   flex-direction: column;
@@ -223,12 +255,6 @@ const Img = styled.img`
 `;
 
 
-
-
-
-
-
-
 /* desktop thw newest info style end */
 const alliancePlan = () => {
   return <PageContainer>
@@ -237,29 +263,32 @@ const alliancePlan = () => {
       <TitleWord>Alliance</TitleWord>
       <Background>
         <Circle>
+
+          <Text1>
+            <Img src={cho} alt="" />
+            <Title2>創立初衷</Title2>
+            <TextForm>將既有的桌球經營 Know How 傳承與系統規劃，改善桌球產業發展，希望透過每位老闆力量，共同團結讓台灣桌球被更多人看見！</TextForm>
+          </Text1>
+
           <Circle1>
-            <Text2>
-              <Img src={cho} alt="" />
-              <Title2>創立初衷</Title2>
-              <TextForm>將既有的桌球經營 Know How 傳承與系統規劃，改善桌球產業發展，希望透過每位老闆力量，共同團結讓台灣桌球被更多人看見！</TextForm>
-            </Text2>
           </Circle1>
 
+          <Text2>
+            <Img src={hha} alt="" />
+            <Title2>計畫簡介</Title2>
+            <TextForm>Let's Play 立志打造跨界聯盟的創新桌球團隊，主軸為三大方向：跨產業合作 X 全齡複合空間 X 運動設計美學。希望共享資源讓台灣桌球產業互利合作，推廣讓全年齡的朋友們愛上桌球，一起體驗桌球的好處和快樂！</TextForm>
+          </Text2>
           <Circle2>
-            <Text1>
-              <Img src={hha} alt="" />
-              <Title2>計畫簡介</Title2>
-              <TextForm>Let's Play 立志打造跨界聯盟的創新桌球團隊，主軸為三大方向：跨產業合作 X 全齡複合空間 X 運動設計美學。希望共享資源讓台灣桌球產業互利合作，推廣讓全年齡的朋友們愛上桌球，一起體驗桌球的好處和快樂！</TextForm>
-            </Text1>
           </Circle2>
 
+
+          <Text3>
+            <Img src={ho} alt="" />
+            <Title2>聯盟分佈</Title2>
+            <TextForm>全台目前分店為 4 家，合作聯盟據點 10 個以上，聯盟球館</TextForm>
+            <TextForm2>陸續招募中！</TextForm2>
+          </Text3>
           <Circle3>
-            <Text2>
-              <Img src={ho} alt="" />
-              <Title2>聯盟分佈</Title2>
-              <TextForm>全台目前分店為 4 家，合作聯盟據點 10 個以上，聯盟球館</TextForm>
-              <TextForm2>陸續招募中！</TextForm2>
-            </Text2>
           </Circle3>
         </Circle>
 
