7435afa2f0c02ec193fa116ce705a4cd52fc98d6
Miffy Wang <4000p049@gmail.com>
2022-05-12 11:54:12 +0800

    fix

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -21,7 +21,6 @@ const StartImg1 = styled.img`
   height:63px;
   margin-top:50px;
   margin-right:20px;
-  margin-top:175px;
   @media screen and (max-width: 768px) {
     margin-left:20px;
     margin-top:140px;
@@ -35,7 +34,6 @@ const StartImg2 = styled.img`
   height:63px;
   margin-top:40px;
   margin-right:20px;
-  margin-top:175px;
 
   @media screen and (max-width: 768px) {
     margin-left:20px;
@@ -48,8 +46,9 @@ const StartImg3 = styled.img`
   width: 63px;
   height:63px;
   margin-top:50px;
-  margin-top:175px;
-  margin-left:100px;
+  margin-right:20px;
+
+
   @media screen and (max-width: 768px) {
     margin-left:40px;
     margin-top:-2px;
@@ -62,8 +61,8 @@ const StartImg4 = styled.img`
   width: 63px;
   height:63px;
   margin-top:50px;
-  margin-top:175px;
-  margin-left:100px;
+  margin-right:20px;
+
   @media screen and (max-width: 768px) {
     margin-left:20px;
     margin-top:60px;
@@ -99,7 +98,6 @@ const RightItems = styled.div`
  display:flex;
  flex-direction: column;
  width:50%;
- margin-right:60px;
  @media screen and (max-width: 768px) {
     
   }
@@ -276,7 +274,6 @@ const LeftAreaContainer = styled.div`
   width:100%;
   display: flex;
   justify-content:center;
-  margin-left:-60px;
   @media screen and (max-width: 768px) {
     display: flex;
     flex-direction: column;
@@ -288,7 +285,6 @@ const RightAreaContainer = styled.div`
   width:50%;
   display: flex;
   justify-content:flex-start;
-  margin-right:80px;
   @media screen and (max-width: 768px) {
     display: flex;
     flex-direction: column;
@@ -333,6 +329,7 @@ const Span = styled.span`
 
 const HopeContainer = styled.div`
   display:flex;
+  justify-content:space-between;
   
   
   
@@ -343,7 +340,8 @@ const HopeContainer = styled.div`
 
 const BrandContainer = styled.div`
   display:flex;
-  margin-top:-30px;
+  justify-content:space-between;
+
   @media screen and (max-width: 768px) {
     
   }
@@ -391,7 +389,12 @@ const TextContainer = styled.div`
  margin-top:-25px;
 `;
 
+const PicturAndText = styled.div`
+display:flex;
+margin-left:-80px;
 
+
+`;
 
 /* desktop thw newest info style end */
 const About = () => {
@@ -405,11 +408,17 @@ const About = () => {
         <LeftAreaContainer>
           <LeftItems>
             <HopeContainer>
-              <StartImg1 src={start} alt="" />
+             
               <TextArea>
-                <Hope1>願景</Hope1>
+                <PicturAndText>
+                  <StartImg1 src={start} alt="" />
+                  <div>
+                    <Hope1>願景</Hope1>
+                    <Slogan>Let's Play <Span>Dream</Span></Slogan>
+                    </div>
+                </PicturAndText>
                 <TextContainer>
-                  <Slogan>Let's Play <Span>Dream</Span></Slogan>
+               
                   <Contants>
                     我們有個小小的願望，就是和大家一起「玩」桌球，透過輕鬆玩，讓全身細胞體會動起來的暢快感！我們有個大大的夢想，藉由「玩」桌球，跨越單純的運動本身、與各種可能性結合，讓桌球走進你我的生活中，帶著爸爸、媽媽、阿公、阿嬤、叔叔、伯伯、阿姨、姑姑、哥哥、姊姊、弟弟、妹妹、朋友所有人一起 Let's Play！
                   </Contants>
@@ -417,13 +426,20 @@ const About = () => {
               </TextArea>
             </HopeContainer>
             <BrandContainer>
-              <StartImg2 src={start} alt="" />
+             
               <TextArea>
-                <Hope2>品牌理念</Hope2>
+                <PicturAndText>
+                  <StartImg2 src={start} alt="" />
+                  <div>
+                    <Hope2>品牌理念</Hope2>
+                    <Slogan>Let's Play <Span>Thinking</Span></Slogan>
+                  </div>
+                </PicturAndText>
+               
                 <TextContainer>
-                  <Slogan>Let's Play <Span>Thinking</Span></Slogan>
+              
                   <Contants>
-                    感受發自內心的快樂，一起來玩吧！
+                    感受發自內心的快樂，一起來玩吧！<br/>
                     品牌的核心價值在於「讓來到 Let's Play 的人能發自內心的感受純粹的快樂」，孩子從中獲得專注力與成就感、上班族重新找回喜愛一件事物的單純與雀躍感、長者獲得豐富退休生活的歸屬感。
                   </Contants>
                 </TextContainer>
@@ -437,14 +453,18 @@ const About = () => {
         <RightAreaContainer>
           <RightItems>
             <HopeContainer>
-              <StartImg3 src={start} alt="" />
               <TextArea2>
-                <Hope3>使命</Hope3>
+                <PicturAndText>
+                  <StartImg3 src={start} alt="" />
+                  <div>
+                    <Hope3>使命</Hope3>
+                    <Slogan>
+                      Let’s Play Goal
+                      <Span>Dream</Span>
+                    </Slogan>
+                  </div>
+                </PicturAndText>
                 <TextContainer>
-                  <Slogan>
-                    Let’s Play Goal 
-                    <Span>Dream</Span>
-                  </Slogan>
                   <Contants>
                     我們的使命就是讓你來玩的每一分鐘，都感受到放鬆與樂趣！來到這裡你可以輕鬆的玩、簡單的玩、隨心的玩，享受純粹的快樂！我們也致力於和不同產業領域聯盟合作，碰撞出創意無限的火花，提升整個桌球教學生態體系、創造新的無限可能。
                   </Contants>
@@ -452,11 +472,17 @@ const About = () => {
               </TextArea2>
             </HopeContainer>
             <BrandContainer>
-              <StartImg4 src={start} alt="" />
+              
               <TextArea>
-                <Hope4>教學理念</Hope4>
                 <TextContainer>
-                <Slogan>Let’s Play Teach <Span>Thinking</Span></Slogan>
+                  <PicturAndText>
+                    <StartImg4 src={start} alt="" />
+                    <div>
+                      <Hope4>教學理念</Hope4>
+                      <Slogan>Let’s Play Teach <Span>Thinking</Span></Slogan>
+                    </div>
+                  </PicturAndText>
+               
                   <Contants>
                     學習桌球這件事情除了提升球技，我們更享受一起參與課程和遊戲的你們，獲得更多生命中的禮物，像是運動家精神、人生體悟、培養品性和歡笑與汗水。一起養成良好的運動習慣，多一種社交的方式，Let's Play 希望打破大家對桌球的認知，原來桌球可以在玩樂中學習成長！
                   </Contants>
