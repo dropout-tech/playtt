732f24e247c620a9040799be4d15b279ffa3011c
Miffy Wang <4000p049@gmail.com>
2022-05-24 21:48:01 +0800

    fix

--- src/components/alliancePlan.tsx
+++ src/components/alliancePlan.tsx
@@ -3,10 +3,10 @@ import styled from "styled-components";
 import cho from "../assets/alliancePlan/cho.png";
 import hha from "../assets/alliancePlan/hha.png";
 import ho from "../assets/alliancePlan/ho.png";
-import phone1 from "../assets/alliancePlan/phone1.png";
 import phone2 from "../assets/alliancePlan/phone2.png";
 import phone3 from "../assets/alliancePlan/phone3.png";
 import BgWeb from "../assets/alliancePlan/BgWeb.png";
+import BgMobile from "../assets/alliancePlan/BgMobile.png";
 // styled components start 
 
 
@@ -19,12 +19,9 @@ const PageContainer = styled.div`
 const Container = styled.div`
  width:100%;
  height:1011px;
-  
- 
  @media screen and (max-width: 768px) {
 display:none;
  }
- 
 `;
 
 const Title = styled.p`
@@ -37,7 +34,6 @@ const Title = styled.p`
   letter-spacing: 0.04em;
   color: #1A1A1A;
   margin:0px;
-
 `;
 
 const TitleWord = styled.p`
@@ -51,7 +47,6 @@ const TitleWord = styled.p`
   color: #CFD2D3;
   margin-top:8px;
   line-Height:52px;
-  
 `;
 
 const Background = styled.div`
@@ -59,46 +54,6 @@ const Background = styled.div`
   width:100%;
   height:80%;
   margin-top:60px;
-  
- 
-  
-`;
-
-
-const Circle1 = styled.div`
-  display:flex;
-  justify-content:center;
-  align-items: center;
-  width: 588px;
-  height: 588px;
-  background: #FFFFFF;
-  border-radius:294px;
-  margin-right:-170px;
-  
-`;
-
-const Circle2 = styled.div`
-  display:flex;
-  justify-content:center;
-  align-items: center;
-  width: 660px;
-  height: 660px;
-  background: #FFFFFF;
-  border-radius:330px;
-
-
-  
-`;
-
-const Circle3 = styled.div`
-  display:flex;
-  justify-content:center;
-  align-items: center;
-  width: 588px;
-  height: 588px;
-  background: #FFFFFF;
-  border-radius:294px;
-  margin-left:-170px;
 `;
 
 
@@ -106,27 +61,21 @@ const Circle3 = styled.div`
 const Text1 = styled.div`
  margin-top:-100px;
  margin-right:130px;
- 
 `;
 
 const Text2 = styled.div`
  margin-top:-100px;
-
- 
 `;
+
 const Text3 = styled.div`
  margin-top:-98px;
  margin-left:130px;
-
-
 `;
 
 const Circle = styled.div`
  display:flex;
  align-items: center;
  justify-content:center;
-
-
 `;
 
 const TextForm = styled.div`
@@ -165,7 +114,6 @@ const TextForm2 = styled.p`
   letter-spacing: 0.04em;
   color: #1A1A1A;
   margin-top:-60px;
-  
 `;
 
 const Img = styled.img`
@@ -173,7 +121,6 @@ const Img = styled.img`
   height:64px;
   margin-top:-10px;
   margin-left:-15px;
-  
 `;
 
 
@@ -326,7 +273,7 @@ const Text33 = styled.div`
 
 `;
 
-const Circle111 = styled.div`
+const MobleCircleDiv = styled.div`
 @media screen and (max-width: 768px) {
   display:flex;
   flex-direction: column;
@@ -336,10 +283,13 @@ const Circle111 = styled.div`
   padding-top:70px;
   margin-top:-150px;
   margin-left:-55px;
-
-}
-
-`;
+  height:965px;
+  background-image: url('${BgMobile}');
+  background-size:contain ;
+  background-repeat: no-repeat;
+  background-position:center;
+ 
+}`;
 
 const Img1 = styled.img`
   @media screen and (max-width: 768px) {
@@ -462,11 +412,11 @@ const TextArea = styled.div`
 
 const BgWebDiv = styled.div`
  height:804px;
- 
  background-image: url('${BgWeb}');
  background-size:contain ;
  background-repeat: no-repeat;
  background-position:center;
+ 
 
 `;
 /* desktop thw newest info style end */
@@ -524,32 +474,26 @@ const alliancePlan = () => {
           <TitleWord2>Alliance</TitleWord2>
         </TitleContainer>
         <Background>
-          <Circle111>
-            <Circle11>
-              <Text11>
-                <Img1 src={phone1} alt="" />
-                <Title22>創立初衷</Title22>
-                <TextForm222>將既有的桌球經營 <Span>Know How </Span>傳承與系統規劃，改善桌球產業發展，希望透過每位老闆力量，共同團結讓台灣桌球被更多人看見！</TextForm222>
-              </Text11>
-            </Circle11>
+          <MobleCircleDiv>
+            <Text11>
+              <Title22>創立初衷</Title22>
+              <TextForm222>將既有的桌球經營 <Span>Know How </Span>傳承與系統規劃，改善桌球產業發展，希望透過每位老闆力量，共同團結讓台灣桌球被更多人看見！</TextForm222>
+            </Text11>
+  
+            <Text22>
+              <Img2 src={phone2} alt="" />
+              <Title22>計畫簡介</Title22>
+              <TextForm222><Span>Let's Play</Span> 立志打造跨界聯盟的創新桌球團隊，主軸為三大方向：跨產業合作 <Span>X </Span>全齡複合空間<Span>X</Span>  運動設計美學。希望共享資源讓台灣桌球產業互利合作，推廣讓全年齡的朋友們愛上桌球，一起體驗桌球的好處和快樂！</TextForm222>
+            </Text22>
 
-            <Circle22>
-              <Text22>
-                <Img2 src={phone2} alt="" />
-                <Title22>計畫簡介</Title22>
-                <TextForm222><Span>Let's Play</Span> 立志打造跨界聯盟的創新桌球團隊，主軸為三大方向：跨產業合作 <Span>X </Span>全齡複合空間<Span>X</Span>  運動設計美學。希望共享資源讓台灣桌球產業互利合作，推廣讓全年齡的朋友們愛上桌球，一起體驗桌球的好處和快樂！</TextForm222>
-              </Text22>
-            </Circle22>
-
-            <Circle33>
-              <Text33>
-                <Img3 src={phone3} alt="" />
-                <Title22>聯盟分佈</Title22>
-                <TextForm222>全台目前分店為 <Span>4</Span> 家，合作聯盟據點 <Span>10</Span> 個以上，聯盟球館</TextForm222>
-                <TextForm2222>陸續招募中！</TextForm2222>
-              </Text33>
-            </Circle33>
-          </Circle111>
+            <Text33>
+              <Img3 src={phone3} alt="" />
+              <Title22>聯盟分佈</Title22>
+              <TextForm222>全台目前分店為 <Span>4</Span> 家，合作聯盟據點 <Span>10</Span> 個以上，聯盟球館</TextForm222>
+              <TextForm2222>陸續招募中！</TextForm2222>
+            </Text33>
+          
+          </MobleCircleDiv>
 
         </Background>
 
